5e23e01e5febc8527e1e3646adfae7e8353e4ccd
Tom Rowe <theflyingcoder@hotmail.com>
2019-11-27 09:34:01 +1000

    Update the server settings

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import Home from './pages/Home';
+import Document from './pages/Document';
 
 export default [
     {
         path: '/',
         exact: true,
         component: Home
+    },
+    {
+        path: '/document',
+        exact: true,
+        component: Document
     },
 ]
